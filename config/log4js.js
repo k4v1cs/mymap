4dc707df767bd7d4ace82747a9b478a29b7eda2e
k4v1cs <katalin.boglarka.balint@gmail.com>
2014-01-26 13:07:54 +0100

    Create logs dir if necessary

--- config/log4js.js
+++ config/log4js.js
@@ -1,21 +1,30 @@
-var log4js = require('log4js');
+var log4js = require('log4js'),
+    fs = require('fs');
 
-log4js.configure({
-  appenders: [
-    { 
-        'type': 'file',
-        'filename': 'logs/mymap.log',
-        'category': ['mymap', 'console'],
-        'maxLogSize': 5242880,
-        'backups': 10
-    },
-    { 
-        type: 'console'
-    }
-  ],
-  replaceConsole: true
-});
+configure();
 
 exports.getLogger = function() {
     return log4js.getLogger('mymap');
+}
+
+function configure() {
+    if(!fs.existsSync('logs')) {
+        fs.mkdirSync('logs');
+    }
+        
+    log4js.configure({
+      appenders: [
+        { 
+            'type': 'file',
+            'filename': 'logs/mymap.log',
+            'category': ['mymap', 'console'],
+            'maxLogSize': 5242880,
+            'backups': 10
+        },
+        { 
+            type: 'console'
+        }
+      ],
+      replaceConsole: true
+    });
 }
